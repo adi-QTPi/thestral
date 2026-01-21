82e3c49c767d801961d1ec4b143b132246d618f3
karma <captainav0608@gmail.com>
2026-01-21 19:28:12 +0530

    [feat] ssl certification with autocert Signed-off-by: karma <captainav0608@gmail.com>

--- internal/proxy/http/server.go
+++ internal/proxy/http/server.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"crypto/tls"
 	"log"
 	"net/http"
 
@@ -8,6 +9,7 @@ import (
 	"github.com/adi-QTPi/thestral/internal/proxy"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 func InitServer(cfg *config.Env, proxy proxy.Service) {
@@ -17,9 +19,33 @@ func InitServer(cfg *config.Env, proxy proxy.Service) {
 
 	router.NotFound(c.handlePublicRequest)
 
+	certManager := &autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist(cfg.HOST_DOMAIN),
+		Cache:      autocert.DirCache("certs"),
+	}
+
+	httpsServer := &http.Server{
+		Addr:    cfg.PROXY_SSL_BIND,
+		Handler: router,
+		TLSConfig: &tls.Config{
+			GetCertificate: certManager.GetCertificate,
+			MinVersion:     tls.VersionTLS12,
+		},
+	}
+
+	// Challenge Handler on HTTP for lets encrypt management
 	uri := cfg.PROXY_BIND
-	log.Println("Public Proxy listening on ", uri)
-	if err := http.ListenAndServe(uri, router); err != nil {
+	go func() {
+		log.Println("ACME Challenge Server listening on ", uri)
+		if err := http.ListenAndServe(uri, certManager.HTTPHandler(nil)); err != nil {
+			log.Fatalf("HTTP challenge server failed: %v", err)
+		}
+	}()
+
+	secureUri := cfg.PROXY_SSL_BIND
+	log.Println("Public HTTPS Proxy listening on ", secureUri)
+	if err := httpsServer.ListenAndServeTLS("", ""); err != nil {
 		log.Fatalf("public proxy server stopped: %v", err)
 	}
 }
